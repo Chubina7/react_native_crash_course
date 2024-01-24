9823da273c8555d92cefa99ca3a45f9fc1382571
Luka Chubinidze <chubinidzel08@gmail.com>
2024-01-24 14:46:33 +0400

    add welcome and navbar ui

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,40 @@
 import { useState } from "react";
-import { Text, View, ScrollView, SafeAreaView } from "react-native";
+import { View, ScrollView, SafeAreaView } from "react-native";
 import { Stack, useRouter } from "expo-router";
 
 import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components/index";
 
 export default function Page() {
+  const router = useRouter();
+
   return (
-    <View>
-      <View>
-        <Text>Hello World</Text>
-        <Text>This is the first page of your app.</Text>
-      </View>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+          ),
+          title: "",
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
